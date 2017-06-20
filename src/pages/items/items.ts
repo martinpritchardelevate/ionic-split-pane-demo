1e36415d6263712509277ad486ba3eea577f63c8
Martin Pritchard <martinpritchard@elevatedirect.com>
2017-06-20 08:39:10 +0100

    Add ionic menu
    ...to demonstrate that this is still possible.

--- src/pages/items/items.ts
+++ src/pages/items/items.ts
@@ -18,16 +18,16 @@ export class ItemsPage extends _MasterPage {
     detailNavCtrl: NavController = null
 
     items: Array<any> = [
-        { id: 0, description: 'Master Item Zero' },
-        { id: 1, description: 'Master Item One' },
-        { id: 2, description: 'Master Item Two' },
-        { id: 3, description: 'Master Item Three' },
-        { id: 4, description: 'Master Item Four' },
-        { id: 5, description: 'Master Item Five' },
-        { id: 6, description: 'Master Item Six' },
-        { id: 7, description: 'Master Item Seven' },
-        { id: 8, description: 'Master Item Eight' },
-        { id: 9, description: 'Master Item Nine' },
+        { id: 0, description: 'List Item Zero' },
+        { id: 1, description: 'List Item One' },
+        { id: 2, description: 'List Item Two' },
+        { id: 3, description: 'List Item Three' },
+        { id: 4, description: 'List Item Four' },
+        { id: 5, description: 'List Item Five' },
+        { id: 6, description: 'List Item Six' },
+        { id: 7, description: 'List Item Seven' },
+        { id: 8, description: 'List Item Eight' },
+        { id: 9, description: 'List Item Nine' },
     ];
 
     constructor(
