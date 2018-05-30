e4d111f8180e42a45ddd46eeb5401c9b6d1f17b2
dorucioclea <doru.cioclea@corebuild.eu>
2018-05-30 13:30:23 +0300

    enable multiple master page navigation (to test)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav } from 'ionic-angular';
+import { Platform, Nav, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { NavProxyService } from '../services/NavProxy.service';
 import { ItemsPage } from '../pages/items/items';
 import { ItemsHerePage } from '../pages/items-here/items-here'
 import { PlaceholderPage } from '../pages/placeholder/placeholder';
+import { _MasterPage } from '../pages';
 
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
+
+    // master pages declaration
+    private ItemsMasterPage : _MasterPage;
+    private ItemsHereMasterPage : _MasterPage;
 
     // Grab References to our 2 NavControllers...
     @ViewChild('detailNav') detailNav: Nav;
@@ -24,9 +29,13 @@ export class MyApp {
         platform: Platform,
         statusBar: StatusBar,
         splashScreen: SplashScreen,
-        private navProxy: NavProxyService) {
+        private navProxy: NavProxyService, 
+        private menu: MenuController) {
 
         platform.ready().then(() => {
+
+            this.ItemsMasterPage = ItemsPage;
+            this.ItemsHereMasterPage = ItemsHerePage;
 
             statusBar.styleDefault();
             splashScreen.hide();
@@ -45,15 +54,10 @@ export class MyApp {
 
     }
 
-    onMenuItemChanged(menuItem){
-
-        if(menuItem === 'ItemsPage'){
-            this.navProxy.pushMaster(ItemsPage);
-        }
-
-        if(menuItem === 'ItemsHerePage'){
-            this.navProxy.pushMaster(ItemsHerePage);
-        }
+    onMenuItemChanged(menuItem : _MasterPage){
+        //console.log(menuItem);
+        this.navProxy.pushMaster(menuItem);
+        this.menu.close();
     }
 }
 
