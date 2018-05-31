9c232a89abeb09336cb958f15f10b5eb9c53b1f6
dorucioclea <doru.cioclea@corebuild.eu>
2018-05-31 13:54:09 +0300

    add master/detail navs in the main page

--- src/pages/main/main.module.ts
+++ src/pages/main/main.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MainPage } from './main';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicErrorHandler } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { NavProxyService } from '../../services/NavProxy.service';
+
+import {
+  ItemsPage,
+  ItemPage,
+  ItemsHerePage,
+  PlaceholderPage } from '../';
 
 @NgModule({
   declarations: [
     MainPage,
+    ItemsPage,
+        ItemPage,
+        ItemsHerePage,
+        PlaceholderPage
   ],
   imports: [
+    BrowserModule,
+        
     IonicPageModule.forChild(MainPage),
   ],
   exports: [
     MainPage
+  ],
+  providers: [
+    NavProxyService,
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class MainPageModule {}
--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav, MenuController } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { NavProxyService } from '../../services/NavProxy.service';
+import { ItemsPage } from '../items/items';
+import { ItemsHerePage } from '../items-here/items-here'
+import { PlaceholderPage } from '../placeholder/placeholder';
+import { _MasterPage } from '../';
 
 /**
  * Generated class for the MainPage page.
@@ -14,11 +22,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MainPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  masterPages: Array<{title: string, icon: string, component: _MasterPage}>;
+
+  // Grab References to our 2 NavControllers...
+  @ViewChild('detailNav') detailNav: Nav;
+  @ViewChild('masterNav') masterNav: Nav;
+
+  // Empty placeholders for the 'master/detail' pages...
+  masterPage: any = null;
+  detailPage: any = null;
+
+  constructor(
+      platform: Platform,
+      statusBar: StatusBar,
+      splashScreen: SplashScreen,
+      private navProxy: NavProxyService, 
+      private menu: MenuController) {
+
+      this.masterPages = [
+          {title : "One", icon: "star", component: ItemsPage},
+          {title: "Two", icon: "appstore", component: ItemsHerePage}
+      ];
+
+      platform.ready().then(() => {
+
+          statusBar.styleDefault();
+          splashScreen.hide();
+
+          // Add our nav controllers to
+          // the nav proxy service...
+          navProxy.masterNav = this.masterNav;
+          navProxy.detailNav = this.detailNav;
+
+          // set initial pages for
+          // our nav controllers...
+          this.masterNav.setRoot(ItemsPage, { detailNavCtrl: this.detailNav });
+          this.detailNav.setRoot(PlaceholderPage);
+
+      });
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MainPage');
+  onMenuItemChanged(menuItem){
+      //console.log(menuItem);
+      this.navProxy.pushMaster(menuItem.component);
+      this.menu.close();
   }
-
 }
